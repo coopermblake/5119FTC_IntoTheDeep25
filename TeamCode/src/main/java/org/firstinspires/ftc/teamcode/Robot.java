2374d3fae1b0ef504c92523e232d036d55d97f9f
Cooper Blake <cblake053@rsdmo.org>
2024-10-15 17:01:51 -0500

    LGTM

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -1,12 +1,70 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot.LogoFacingDirection;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot.UsbFacingDirection;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.IMU;
 
 public class Robot {
-    public Robot(
-            DcMotor backLeft, DcMotor backRight, DcMotor frontLeft, DcMotor frontRight, IMU imu
-    ) {
+    DcMotor backLeft;
+    DcMotor backRight;
+    DcMotor frontLeft;
+    DcMotor frontRight;
+    DcMotor slideExt;
+    DcMotor slideRot;
+    IMU imu;
+    HardwareMap hardwareMap;
+    Gamepad gamepad1;
+    Gamepad gamepad2;
+    float[] driveMotorPower;
+
+    public Robot(HardwareMap HardwareMap, Gamepad Gamepad1, Gamepad Gamepad2) {
+        hardwareMap = HardwareMap;
+        gamepad1 = Gamepad1;
+        gamepad2 = Gamepad2;
+    }
+    public void init() {
+        backLeft = hardwareMap.get(DcMotor.class, "NAME");
+        backRight = hardwareMap.get(DcMotor.class, "NAME");
+        frontLeft = hardwareMap.get(DcMotor.class, "NAME");
+        frontRight = hardwareMap.get(DcMotor.class, "NAME");
+        slideExt = hardwareMap.get(DcMotor.class, "NAME");
+        slideRot = hardwareMap.get(DcMotor.class, "NAME");
+        imu = hardwareMap.get(IMU.class, "NAME");
+
+        backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        imu.initialize(new IMU.Parameters(new RevHubOrientationOnRobot(LogoFacingDirection.LEFT, UsbFacingDirection.UP)));
+    }
+    public void processDriveInput() {
+        // 2 3
+        // 0 1
+        float inputY = -gamepad1.left_stick_y;
+        float inputX = gamepad1.left_stick_x;
+        float inputRot = gamepad1.right_stick_x;
+        float divisor = Math.max(Math.abs(inputY) + Math.abs(inputX) + Math.abs(inputRot), 1); // Scales movement
+        driveMotorPower[0] = (inputY - inputX + inputRot)/divisor;
+        driveMotorPower[1] = (inputY + inputX - inputRot)/divisor;
+        driveMotorPower[2] = (inputY + inputX + inputRot)/divisor;
+        driveMotorPower[3] = (inputY - inputX - inputRot)/divisor;
+    }
+    public void rawDriveMovement() {
+        backLeft.setPower(driveMotorPower[0]);
+        backRight.setPower(driveMotorPower[1]);
+        frontLeft.setPower(driveMotorPower[2]);
+        frontRight.setPower(driveMotorPower[3]);
+    }
+    public void PIDAdjustedDriveMovement() {
 
     }
+
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testModes.java
@@ -0,0 +1,65 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name="MotorTest",group="TEST")
+public class testModes extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        Robot robot = new Robot(hardwareMap, gamepad1, gamepad2);
+        robot.init();
+        waitForStart();
+        while(opModeIsActive()) {
+            telemetry.addData("backLeft encoder value:", robot.backLeft.getCurrentPosition());
+            telemetry.addData("backRight encoder value:", robot.backRight.getCurrentPosition());
+            telemetry.addData("frontLeft encoder value:", robot.frontLeft.getCurrentPosition());
+            telemetry.addData("frontRight encoder value:", robot.frontRight.getCurrentPosition());
+            telemetry.addData("slideExt encoder value:", robot.slideExt.getCurrentPosition());
+            telemetry.addData("slideRot encoder value:", robot.slideRot.getCurrentPosition());
+
+            // Mappings for drivetrain:
+            //  FRONT
+            // (Y) (B)
+            // (X) (A)
+            //  BACK
+            if(gamepad1.x) {
+                robot.backLeft.setPower(1);
+            } else {
+                robot.backLeft.setPower(0);
+            }
+            if(gamepad1.a) {
+                robot.backRight.setPower(1);
+            } else {
+                robot.backRight.setPower(0);
+            }
+            if(gamepad1.y) {
+                robot.frontLeft.setPower(1);
+            } else {
+                robot.frontLeft.setPower(0);
+            }
+            if(gamepad1.b) {
+                robot.frontRight.setPower(1);
+            } else {
+                robot.frontRight.setPower(0);
+            }
+            // Mappings for slide:
+            // (left_bumper) OUT - IN (right_bumper)
+            // dpad up/down = slide rotate up/down
+            if(gamepad1.left_bumper) {
+                robot.slideExt.setPower(0.5);
+            } else if(gamepad1.right_bumper) {
+                robot.slideExt.setPower(-0.5);
+            } else {
+                robot.slideExt.setPower(0);
+            }
+            if(gamepad1.dpad_up) {
+                robot.slideRot.setPower(0.5);
+            } else if(gamepad1.dpad_down) {
+                robot.slideRot.setPower(-0.5);
+            } else {
+                robot.slideRot.setPower(0);
+            }
+        }
+    }
+}
