5d368ecadd1e187e83ed5be19626e4b108fb7ef2
Cooper Blake <cblake053@rsdmo.org>
2024-10-20 16:42:37 -0500

    Add additional hardware to the test opmode + slow things down slightly

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testMode.java
@@ -11,43 +11,48 @@ public class testMode extends LinearOpMode {
         robot.init();
         waitForStart();
         while(opModeIsActive()) {
+            // note: encoder for backLeft is currently broken
             telemetry.addData("backLeft encoder value:", robot.backLeft.getCurrentPosition());
             telemetry.addData("backRight encoder value:", robot.backRight.getCurrentPosition());
             telemetry.addData("frontLeft encoder value:", robot.frontLeft.getCurrentPosition());
             telemetry.addData("frontRight encoder value:", robot.frontRight.getCurrentPosition());
+            telemetry.addData("slideExt encoder value:", robot.slideExt.getCurrentPosition());
+            telemetry.addData("slideRot encoder value:", robot.slideRot.getCurrentPosition());
             telemetry.update();
-            //telemetry.addData("slideExt encoder value:", robot.slideExt.getCurrentPosition());
-            //telemetry.addData("slideRot encoder value:", robot.slideRot.getCurrentPosition());
 
             // Mappings for drivetrain:
             //  FRONT
             // (Y) (B)
             // (X) (A)
             //  BACK
+            // Y = BL
+            // B = BR
+            // X = FL
+            // A = FR
             if(gamepad1.x) {
-                robot.backLeft.setPower(1);
+                robot.backLeft.setPower(0.5);
             } else {
                 robot.backLeft.setPower(0);
             }
             if(gamepad1.a) {
-                robot.backRight.setPower(1);
+                robot.backRight.setPower(0.5);
             } else {
                 robot.backRight.setPower(0);
             }
             if(gamepad1.y) {
-                robot.frontLeft.setPower(1);
+                robot.frontLeft.setPower(0.5);
             } else {
                 robot.frontLeft.setPower(0);
             }
             if(gamepad1.b) {
-                robot.frontRight.setPower(1);
+                robot.frontRight.setPower(0.5);
             } else {
                 robot.frontRight.setPower(0);
             }
             // Mappings for slide:
-            // (left_bumper) OUT - IN (right_bumper)
+            // (left_bumper) OUT | IN (right_bumper)
             // dpad up/down = slide rotate up/down
-            /* if(gamepad1.left_bumper) {
+            if(gamepad1.left_bumper) {
                 robot.slideExt.setPower(0.5);
             } else if(gamepad1.right_bumper) {
                 robot.slideExt.setPower(-0.5);
@@ -61,7 +66,6 @@ public class testMode extends LinearOpMode {
             } else {
                 robot.slideRot.setPower(0);
             }
-            */
         }
     }
 }
