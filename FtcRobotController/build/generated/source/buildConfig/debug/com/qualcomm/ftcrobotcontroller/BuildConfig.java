26014beef7b05f683934e3bd2da9d5f34f46603c
Cooper Blake <cblake053@rsdmo.org>
2024-10-20 16:44:11 -0500

    Edit build config

--- FtcRobotController/build/generated/source/buildConfig/debug/com/qualcomm/ftcrobotcontroller/BuildConfig.java
+++ FtcRobotController/build/generated/source/buildConfig/debug/com/qualcomm/ftcrobotcontroller/BuildConfig.java
@@ -8,5 +8,5 @@ public final class BuildConfig {
   public static final String LIBRARY_PACKAGE_NAME = "com.qualcomm.ftcrobotcontroller";
   public static final String BUILD_TYPE = "debug";
   // Field from default config.
-  public static final String APP_BUILD_TIME = "2024-10-15T17:43:18.850-0500";
+  public static final String APP_BUILD_TIME = "2024-10-17T17:00:13.744-0500";
 }
