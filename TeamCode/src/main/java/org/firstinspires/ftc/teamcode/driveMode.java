46e43dbd82be350ec0c24c4ab1a6ce04ac0e74d7
baryons5119ftc <baryons5119ftc@gmail.com>
2025-01-13 18:26:31 -0600

    changed TuningOpModes stuff for 3 wheel odometry

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/driveMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/driveMode.java
@@ -1,25 +1,24 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.acmerobotics.dashboard.FtcDashboard;
-import com.qualcomm.hardware.motors.GoBILDA5201Series;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-
-import org.firstinspires.ftc.robotcore.internal.network.WifiDirectAgent;
-import org.firstinspires.ftc.robotcore.internal.opengl.models.Geometry;
 
 @TeleOp(name="Drive Mode",group="Competition Opmodes")
 public class driveMode extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
         Robot robot = new Robot(hardwareMap, gamepad1, gamepad2);
+        double heading;
+
         long lastCycleStart = System.currentTimeMillis();
         waitForStart();
         robot.viperSlide.driverControl = true;
         while(opModeIsActive()) {
             telemetry.addData("refresh rate (hz): ",1000/(System.currentTimeMillis() - lastCycleStart + 1));
             lastCycleStart = System.currentTimeMillis();
-            robot.drivetrain.robotCentricDrive(gamepad1, gamepad2);
+            heading = robot.getYawDegrees();
+            robot.drivetrain.driveRobot(gamepad1, gamepad2, heading);
             robot.viperSlide.teleopSlideMovement(gamepad1, gamepad2);
+            telemetry.addData("field centric", robot.drivetrain.getFieldCentric());
             telemetry.addData("ext", robot.viperSlide.slideExt.getCurrentPosition());
             telemetry.addData("rot", robot.viperSlide.slideRot.getCurrentPosition());
             telemetry.addData("extMin", robot.viperSlide.extMin);
